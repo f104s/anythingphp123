7d0d412754cff2ffaf9ace0650af0f322a2b67e8
daniil <dannymordovin@gmail.com>
2023-09-26 20:48:45 +0500

    task 1

---
+++ Framework/Application.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Framework;
+
+
+#use Framework\Router;
+
+class Application
+{
+
+    private Router $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+
+    public function run(): void
+    {
+        echo print_r($this->router->getContent(), true);
+    }
+}
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+use Framework\Container;
+
+if ( file_exists(dirname(__FILE__).'/vendor/autoload.php') ) {
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
+}
+
+
+Container::getApp()->run();
+
+
+die();
